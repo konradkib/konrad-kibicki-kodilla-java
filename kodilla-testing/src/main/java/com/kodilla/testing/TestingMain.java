e1842a838b3f03ffce481500633d0dda98e79569
konradkib <konrad.kibicki@gmail.com>
2019-04-14 21:48:55 +0200

    TestCalculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,10 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.user.SimpleUser;
-import com.kodilla.testing.SimpleCalculator.Calculator;
+import com.kodilla.testing.calculator.Calculator;
+import java.util.*;
 
-import java.util.Random;
+
 
 public class TestingMain {
 
@@ -17,6 +18,26 @@ public class TestingMain {
             System.out.println("Test ok");
         else
             System.out.println("error");
+
+        System.out.println("------------------ NEW TEST -------------------------" );
+
+        Random random = new Random();
+
+        int a = random.nextInt(100);
+        int b = random.nextInt(100);
+        Calculator calculator = new Calculator(a,b);
+
+        int AddResult = calculator.addAToB();
+        int SubResult = calculator.subtractAToB();
+
+        if((AddResult == (a+b)) || (SubResult == (a-b)))
+            System.out.println("Test ok");
+        else if(AddResult != (a+b))
+            System.out.println("Add method wrong result");
+        else if(SubResult != (a-b))
+            System.out.println("Sub method wrong result");
+        else
+            System.out.println("Both methods works wrong");
 
 
 
