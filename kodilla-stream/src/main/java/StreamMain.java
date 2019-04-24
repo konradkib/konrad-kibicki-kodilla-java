b11ae75f26412e53528b9b1ad76a7d7036b19773
konradkib <konrad.kibicki@gmail.com>
2019-04-24 16:40:15 +0200

    Lambda

--- kodilla-stream/src/main/java/StreamMain.java
+++ kodilla-stream/src/main/java/StreamMain.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+package com.kodilla-stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
 
 public class StreamMain {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("sample text",text ->text.toUpperCase());
+        poemBeautifier.beautify("text",text ->"ABC"+text+"ABC");
+        poemBeautifier.beautify("            sample      ",text ->text.trim());
+        poemBeautifier.beautify("SUNNY DAY",text ->text.toLowerCase());
+
 }
