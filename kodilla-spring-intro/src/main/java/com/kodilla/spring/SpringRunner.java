17e926b0efbec7ba1c481556de01dc95a56b3fe5
konradkib <konrad.kibicki@gmail.com>
2019-09-05 20:45:14 +0200

    Spring_Forum_User

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -0,0 +1,18 @@
+package com.kodilla.spring;
+
+import com.kodilla.spring.forum.ForumUser;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class SpringRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRunner.class, args);
+
+
+    }
+
+}
